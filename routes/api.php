e51b2aead0993bec1f3326e00a4cbba75ea36ed1
frank <franktrade123@gmail.com>
2023-08-26 15:50:02 +0100

    second change

--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,25 @@
 <?php
 
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\AuthController; // Corrected the class name
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 // Public Routes
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/products', [productcontroller ::class, 'index']); // Corrected the class name
-Route::get('/products/{id}', [productcontroller::class, 'show']); // Corrected the class name
-Route::get('/products/search/{name}', [productcontroller::class, 'search']); // Corrected the class name
+Route::post('/login', [AuthController::class, 'login']); // Add middleware here
+
+Route::get('/products', [ProductController::class, 'index']); // Corrected class name
+Route::get('/products/{id}', [ProductController::class, 'show']); // Corrected class name
+Route::get('/products/aut{name}', [ProductController::class, 'search']); // Corrected class name
 
 // Protected Routes
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/products', [productcontroller::class, 'store']); // Corrected the class name
-    Route::put('/products/{id}', [productcontroller::class, 'update']); // Corrected the class name
-    Route::delete('/products/{id}', [productcontroller::class, 'destroy']); // Corrected the class name
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-}); 
+    Route::post('/products', [ProductController::class, 'store']); // Corrected class name
+    Route::put('/products/{id}', [ProductController::class, 'update']); // Corrected class name
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']); // Corrected class name
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
